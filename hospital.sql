ea6d72fdb1c881dc500cfe65239f470b291611ea
Samuel Toledo Hdez <92159124+SamuelToledoHdez@users.noreply.github.com>
2023-12-15 22:13:02 +0100

    Update hospital.sql

--- hospital.sql
+++ hospital.sql
@@ -14,37 +14,52 @@ CREATE TABLE IF NOT EXISTS Pacientes (
 
 -- Crear la tabla de citas médicas
 CREATE TABLE IF NOT EXISTS Citas (
-    
+    CodigoCita INT PRIMARY KEY AUTO_INCREMENT,
+    Fecha DATE,
+    Motivo VARCHAR(255)
 );
 
 -- Crear la tabla de personal médico
 CREATE TABLE IF NOT EXISTS Medico (
+    NumeroColegiado INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Especialidad VARCHAR(255)
     
 );
 -- Crear la tabla de Enfermero
 CREATE TABLE IF NOT EXISTS Enfermero (
-    
+    DNI VARCHAR(20) PRIMARY KEY,
+    Nombre VARCHAR(100)
 );
 
 -- Crear la tabla de RecetaMedica
 CREATE TABLE IF NOT EXISTS RecetaMedica (
-    
+    CodigoReceta INT PRIMARY KEY,
+    Fecha DATE
 );
 
 -- Crear la tabla de Medicamento
 CREATE TABLE IF NOT EXISTS Medicamento (
-    
+    CodigoMedicamento INT PRIMARY KEY,
+    Tipo VARCHAR(50),
+    Precio DECIMAL(10, 2),
+    Nombre VARCHAR(100)
 );
 
 
 -- Crear la tabla de equipos médicos
 CREATE TABLE IF NOT EXISTS EquiposMedicos (
-    
+    CodigoEquipo INT PRIMARY KEY,
+    Tipo VARCHAR(50),
+    Precio DECIMAL(10, 2),
+    Nombre VARCHAR(100)
 );
 
 -- Crear la tabla de unidades médicas
 CREATE TABLE IF NOT EXISTS UnidadesMedicas (
-    
+    CodigoUnidad INT PRIMARY KEY,
+    Especialidad VARCHAR(50),
+    Localizacion VARCHAR(255),
 );
 
 
