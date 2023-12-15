6aef4414f5d1d6c8549ed8df533dd3b959ac3b47
Samuel Toledo Hdez <92159124+SamuelToledoHdez@users.noreply.github.com>
2023-12-15 21:35:52 +0100

    Create hospital.sql

---
+++ hospital.sql
@@ -0,0 +1,46 @@
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS HospitalXYZ;
+USE HospitalXYZ;
+
+-- Crear la tabla de pacientes
+CREATE TABLE IF NOT EXISTS Pacientes (
+    
+);
+
+-- Crear la tabla de citas médicas
+CREATE TABLE IF NOT EXISTS Citas (
+    
+);
+
+-- Crear la tabla de personal médico
+CREATE TABLE IF NOT EXISTS Medico (
+    
+);
+-- Crear la tabla de Enfermero
+CREATE TABLE IF NOT EXISTS Enfermero (
+    
+);
+
+-- Crear la tabla de RecetaMedica
+CREATE TABLE IF NOT EXISTS RecetaMedica (
+    
+);
+
+-- Crear la tabla de Medicamento
+CREATE TABLE IF NOT EXISTS Medicamento (
+    
+);
+
+
+-- Crear la tabla de equipos médicos
+CREATE TABLE IF NOT EXISTS EquiposMedicos (
+    
+);
+
+-- Crear la tabla de unidades médicas
+CREATE TABLE IF NOT EXISTS UnidadesMedicas (
+    
+);
+
+
+-- Fin del script
