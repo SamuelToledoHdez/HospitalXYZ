1cb27dd4c08a456c21ec6347b201c13ae92453b8
Samuel Toledo Hdez <92159124+SamuelToledoHdez@users.noreply.github.com>
2023-12-16 19:59:38 +0000

    Update hospital.sql

--- hospital.sql
+++ hospital.sql
@@ -1,35 +1,23 @@
-DROP DATABASE IF EXISTS HospitalXYZ;
-DROP TABLE IF EXISTS Pacientes;
+
+DROP DATABASE IF EXISTS hospitalxyz;
 DROP TABLE IF EXISTS Citas;
-DROP TABLE IF EXISTS Medico;
+
 DROP TABLE IF EXISTS Enfermero;
-DROP TABLE IF EXISTS RecetaMedica;
+
 DROP TABLE IF EXISTS Medicamento;
 DROP TABLE IF EXISTS EquiposMedicos;
 DROP TABLE IF EXISTS UnidadesMedicas;
+DROP TABLE IF EXISTS Pacientes;
 
-CREATE DATABASE HospitalXYZ;
+DROP TABLE IF EXISTS RecetaMedica;
+DROP TABLE IF EXISTS Medico;
+
+CREATE DATABASE hospitalxyz;
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- Conectar a la base de datos recién creada
-\c HospitalXYZ;
+\c hospitalxyz;
 
--- Crear la tabla de pacientes
-CREATE TABLE IF NOT EXISTS Pacientes (
-    NombreCompleto VARCHAR(100),
-    DNI_Cifrado TEXT,
-    HistorialMedico TEXT,
-    Telefono1 VARCHAR(20),
-    FechaNacimiento DATE,
-    Edad INT
-);
-
--- Crear la tabla de citas médicas
-CREATE TABLE IF NOT EXISTS Citas (
-    CodigoCita SERIAL PRIMARY KEY,
-    Fecha DATE,
-    Motivo VARCHAR(255)
-);
 
 -- Crear la tabla de personal médico
 CREATE TABLE IF NOT EXISTS Medico (
@@ -37,6 +25,28 @@ CREATE TABLE IF NOT EXISTS Medico (
     Nombre VARCHAR(100),
     Especialidad VARCHAR(255)
 );
+-- Crear la tabla de pacientes
+CREATE TABLE IF NOT EXISTS Pacientes (
+    NombreCompleto VARCHAR(100),
+    DNI_Cifrado TEXT PRIMARY KEY,
+    HistorialMedico TEXT,
+    Telefono1 VARCHAR(20),
+    FechaNacimiento DATE,
+    Edad INT,
+    NumeroColegiado INT REFERENCES medico(NumeroColegiado)
+
+
+);
+
+-- Crear la tabla de citas médicas
+CREATE TABLE IF NOT EXISTS Citas (
+    CodigoCita SERIAL PRIMARY KEY,
+    Fecha DATE,
+    Motivo VARCHAR(255),
+    DNI_Cifrado TEXT REFERENCES Pacientes(DNI_Cifrado)
+);
+
+
 
 -- Crear la tabla de Enfermero
 CREATE TABLE IF NOT EXISTS Enfermero (
@@ -47,7 +57,8 @@ CREATE TABLE IF NOT EXISTS Enfermero (
 -- Crear la tabla de RecetaMedica
 CREATE TABLE IF NOT EXISTS RecetaMedica (
     CodigoReceta SERIAL PRIMARY KEY,
-    Fecha DATE
+    Fecha DATE,
+    NumeroColegiado INT REFERENCES Medico(NumeroColegiado)
 );
 
 -- Crear la tabla de Medicamento
@@ -55,7 +66,9 @@ CREATE TABLE IF NOT EXISTS Medicamento (
     CodigoMedicamento SERIAL PRIMARY KEY,
     Tipo VARCHAR(50),
     Precio DECIMAL(10, 2),
-    Nombre VARCHAR(100)
+    Nombre VARCHAR(100),
+    Codigo_receta SERIAL REFERENCES RecetaMedica(CodigoReceta)
+
 );
 
 -- Crear la tabla de equipos médicos
@@ -65,13 +78,26 @@ CREATE TABLE IF NOT EXISTS EquiposMedicos (
     Precio DECIMAL(10, 2),
     Nombre VARCHAR(100)
 );
+-- Crear la tabla de hospitales
+CREATE TABLE IF NOT EXISTS Hospital (
+    Nombre TEXT,
+    Localizacion TEXT,
+    PRIMARY KEY (Nombre, Localizacion),
+    UNIQUE(Nombre),
+    UNIQUE(Localizacion)
+);
 
 -- Crear la tabla de unidades médicas
 CREATE TABLE IF NOT EXISTS UnidadesMedicas (
     CodigoUnidad SERIAL PRIMARY KEY,
     Especialidad VARCHAR(50),
-    Localizacion VARCHAR(255)
+    Localizacion VARCHAR(255),
+    Nombre_hospital TEXT,
+    Localizacion_hospital TEXT,
+    FOREIGN KEY (Nombre_hospital, Localizacion_hospital) REFERENCES Hospital(Nombre, Localizacion)
 );
+
+
 
 -------------------------------------------------------------------------------------
 
@@ -91,12 +117,17 @@ BEFORE INSERT ON "pacientes"
 FOR EACH ROW
 EXECUTE FUNCTION CifrarDNITriggerFunction();
 
--- Crear la función de descifrado
 CREATE OR REPLACE FUNCTION DescifrarDNI(dni_cifrado TEXT)
 RETURNS TEXT AS $$
 BEGIN
-  -- Utilizar la función de descifrado pgp_sym_decrypt_bytea con la clave secreta
-  RETURN encode(pgp_sym_decrypt_bytea(dni_cifrado::bytea, 'clave_secreta'), 'escape')::text;
+    -- Utilizar la función de descifrado pgp_sym_decrypt_bytea con la clave secreta
+    BEGIN
+        RETURN encode(pgp_sym_decrypt_bytea(dni_cifrado::bytea, 'clave_secreta'), 'escape')::text;
+    EXCEPTION
+        WHEN OTHERS THEN
+            -- Manejar cualquier excepción que pueda surgir al descifrar
+            RAISE EXCEPTION 'Error al descifrar el DNI_Cifrado (%): %', dni_cifrado, SQLSTATE;
+    END;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -122,10 +153,43 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
------------------------------------------------------------------------------------------------
 
-INSERT INTO "pacientes" (nombrecompleto, dni_cifrado, historialmedico, telefono1, fechanacimiento)
-VALUES ('Nombre Apellido', '12345678', 'Historial de prueba', '123456789', '2000-01-01');
+-- Crear el trigger
+CREATE OR REPLACE FUNCTION validar_medico_existente()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Medico WHERE NumeroColegiado = NEW.NumeroColegiado) THEN
+        RAISE EXCEPTION 'El médico con el número colegiado % no existe.', NEW.NumeroColegiado;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Asociar el trigger a la tabla Pacientes
+CREATE TRIGGER tr_validar_medico_existente
+BEFORE INSERT ON Pacientes
+FOR EACH ROW
+EXECUTE FUNCTION validar_medico_existente();
+
+-----------------------------------------------------------------------------------------------
+-- Insertar médicos
+INSERT INTO Medico (NumeroColegiado, Nombre, Especialidad) VALUES
+(1, 'Dr. Juan Pérez', 'Cardiología'),
+(2, 'Dra. María García', 'Pediatría'),
+(3, 'Dr. Carlos Rodríguez', 'Cirugía General');
+
+-- Insertar pacientes
+INSERT INTO Pacientes (NombreCompleto, DNI_Cifrado, HistorialMedico, Telefono1, FechaNacimiento, Edad, NumeroColegiado) VALUES
+('Ana Martínez', '12345678A', 'Historial de Ana', '123-456-7890', '1990-05-15', 32, 1),
+('José Gómez', '98765432B', 'Historial de José', '987-654-3210', '1985-08-22', 37, 2),
+('Laura Fernández', '56789012C', 'Historial de Laura', '555-123-4567', '2000-11-10', 22, 3);
+
+-- Insertar citas con DNI_Cifrado válido
+INSERT INTO Citas (Fecha, Motivo, DNI_Cifrado) VALUES
+('2023-12-20', 'Consulta rutinaria', '12345678A');
+
+
+-- Intentar insertar una cita con DNI_Cifrado no existente (debería lanzar una excepción)
 
 SELECT * FROM Pacientes;
 SELECT * FROM ConsultarPaciente();
